d00c0b56a6b4934aed135a4fed6d5111497d20a6
zakzakst <e1433104@gmail.com>
2024-04-30 19:43:48 +0900

    修正

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import vituum from "vituum";
 import nunjucks from "@vituum/vite-plugin-nunjucks";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   plugins: [vituum(), nunjucks()],
+  resolve: {
+    alias: {
+      "@/": `${__dirname}/src/`,
+    },
+  },
 };
