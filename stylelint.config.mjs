ebc71887241e886c2c69c25be7e0e9f03f6cbaf2
zakzakst <e1433104@gmail.com>
2024-05-02 12:55:24 +0900

    stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,16 @@
+export default {
+  extends: [
+    "stylelint-config-recommended",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-recess-order",
+  ],
+  rules: {
+    "color-hex-length": "short",
+    "no-duplicate-selectors": null,
+    "no-empty-source": null,
+    "no-descending-specificity": null,
+    "scss/operator-no-unspaced": null,
+    "scss/load-no-partial-leading-underscore": null,
+  },
+  ignoreFiles: ["src/sass/**/_*scss", "src/sass/global/mixins.scss"],
+};
